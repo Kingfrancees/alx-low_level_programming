1d85a0b52fcb3d645e658d2e29ef7697cff9c06a
kingfrancees <francisoforkansi58@gmail.com>
2022-10-16 08:13:11 -0700

    commission

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: (0) SUCCESS
+ * description: each output is a random numbe * r and tells if uts -ve or +ve
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+			printf("%d is zero\n", n);
+		}
+return (0);
+}
