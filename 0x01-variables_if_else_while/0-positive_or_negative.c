98bbcf16b6b1d4d5ee3af48cb6cff3cf1ba083d5
kingfrancees <francisoforkansi58@gmail.com>
2022-10-16 08:32:15 -0700

    commission

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
+	if (n == 0)
+	{
+		print("%d is zero\n", n);
+	}
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
@@ -21,9 +25,5 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else
-	{
-			printf("%d is zero\n", n);
-		}
 return (0);
 }
