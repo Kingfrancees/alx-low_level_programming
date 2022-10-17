140adc83b92fe3d6c33aa5ddff2173a695b3fbe3
kingfrancees <francisoforkansi58@gmail.com>
2022-10-16 17:48:15 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: c programming for life
+ * Return: (0) SUCCESS
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+		putchar('\n');
+	return (0);
+}
