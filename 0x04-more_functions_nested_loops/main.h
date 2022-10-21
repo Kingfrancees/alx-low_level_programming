76412e0e21cab70ab66f73ef2cd44d834f758dc3
kingfrancees <francisoforkansi58@gmail.com>
2022-10-21 16:17:56 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_INCLUDE
+
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
